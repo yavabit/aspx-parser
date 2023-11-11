f81fb249e1578b489c58e871ad6c98e6ed37245b
Anton Zhilenkov <a.zhilenkov@globexit.ru>
2023-11-11 17:50:58 +0300

    restructure

--- aspx-parser1.py
+++
@@ -1,5 +0,0 @@
-from core.core import AspxParser
-regExp = r'<mso:(.*?)\s+msdt:dt="string">(.*?)<\/mso:\1>'
-
-parser = AspxParser("C:/Developments/python/aspx-parser/files/**/*.aspx",'-', regExp)
-parser.save_data("output.json")
